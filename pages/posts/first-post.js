e009c8073bee8699b108af027439a6ebf298397c
wyznozy <mchaudev@gmail.com>
2024-03-13 19:41:20 -0500

    3-13 initial commit
    Updated first page to now bring us back to home dashboard

---
+++ pages/posts/first-post.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Link from 'next/link';
+
+export default function FirstPost () {
+  return(
+    <>
+      <h1>
+        First Post!
+      </h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
+}
