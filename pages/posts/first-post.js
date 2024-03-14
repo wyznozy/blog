a7af0d02464f50efef5ba0a3f45507fc7e5979f9
wyznozy <mchaudev@gmail.com>
2024-03-13 20:46:35 -0500

    3-13 update on Styles and adding Image
    Added linkedin profile picture and worked on applying styles to div.
    Also looked at introducing components

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,15 +1,39 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
+import Head from 'next/head';
+import Script from 'next/script'
+import Layout from '../../components/layout';
+
+const MyLinkedInImage = () => (
+  <Image
+    src="/images/profile.jpg"
+    height={400}
+    width={400}
+    alt="Mike C"
+  />
+)
+
+
 
 export default function FirstPost () {
   return(
-    <>
-      <h1>
-        First Post!
-      </h1>
+    // Head is what the tab header is displaying.
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={ () =>
+          console.log('script loaded correctly, window.FB has been populated')
+        }
+      />
+      <h1>First Post!</h1>
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
-    </>
+    </Layout>
   );
 }
